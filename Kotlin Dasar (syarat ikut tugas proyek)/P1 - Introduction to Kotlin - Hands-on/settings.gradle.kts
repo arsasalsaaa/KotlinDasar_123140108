0b25e7e1b786989bd7dbf2f866b6b8aad48de5a6
tomat <arsa.salsabila29@gmail.com>
2026-09-20 22:46:58 +0700

    P1: add hands-on solutions

--- Kotlin Dasar (syarat ikut tugas proyek)/P1 - Introduction to Kotlin - Hands-on/settings.gradle.kts
+++ Kotlin Dasar (syarat ikut tugas proyek)/P1 - Introduction to Kotlin - Hands-on/settings.gradle.kts
@@ -1,3 +1,11 @@
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "2.3.10"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 rootProject.name = "p1-introduction-to-kotlin"
 
 include(
